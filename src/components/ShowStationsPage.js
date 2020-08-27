98a07ff9b7af5ca3aacec0acdf85fd4af45502e3
HB <harshay.imag3@gmail.com>
2020-08-28 03:07:32 +0530

    + basic sorting implementation

--- src/components/ShowStationsPage.js
+++ src/components/ShowStationsPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {getAllStations, getStationStatus} from "../services/stationService"
+import {Checkbox} from "@blueprintjs/core"
 
 class ShowStationsPage extends React.Component {
   constructor(props) {
@@ -30,14 +31,54 @@ class ShowStationsPage extends React.Component {
     stations = stations?.data?.stations;
     status = status?.data?.stations;
     this.mergeStationData(stations, status);
+  }
 
+  handleNameSort = () => {
+    this.setState({nameSort: !this.state.nameSort})
+  }
+
+  handleAvailabilitySort = () => {
+    this.setState({availabilitySort: !this.state.availabilitySort})
+  }
+
+  sortByName = (stations) => {
+    let sortedStations = stations.sort((a, b) => {
+      let nameA = a.name.toUpperCase();
+      let nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+    return sortedStations;
+  }
+
+  sortByAvailability = (stations) => {
+    return stations.sort((a, b) => a.num_bikes_available - b.num_bikes_available);
   }
 
   render() {
-    const stations = this.state?.stations;
+    const { stations, nameSort, availabilitySort } = this.state;
+    let sortedStations = [...stations];
+    if(nameSort) {
+      sortedStations = this.sortByName(sortedStations);
+    }
+    if(availabilitySort) {
+      sortedStations = this.sortByAvailability(sortedStations);
+    }
+
     return (
       <div className={'page-container'}>
-        <h2>Bike Stations (Toronto)</h2>
+        <div className={'table-title'}>
+          <h2>Bike Stations (Toronto)</h2>
+          <Checkbox checked={nameSort} label="Name (Ascending only)" onChange={this.handleNameSort} />
+          <Checkbox checked={availabilitySort} label="Availability (Ascending only)" onChange={this.handleAvailabilitySort} />
+        </div>
 
         <table className="bp3-html-table">
           <thead>
@@ -49,7 +90,7 @@ class ShowStationsPage extends React.Component {
           </thead>
           <tbody>
           {
-            stations && stations.map(item => (
+            sortedStations && sortedStations.map(item => (
               <tr key={item.station_id}>
                 <td>{item.name}</td>
                 <td>{item.capacity}</td>
